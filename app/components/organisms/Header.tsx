4ee08e34eb669a4dd1bf7a31861a6c4fc6bb85d2
DIego <mendesdiego90@gmail.com>
2025-04-12 12:46:19 +0900

    adiciona as pastas do components e cria o arquivo de header.

---
+++ app/components/organisms/Header.tsx
@@ -0,0 +1,16 @@
+
+export default function Header() {
+    return (
+      <header>
+        <div>logo</div>
+        <nav>
+          <ul>            
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
